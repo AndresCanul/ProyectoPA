1cd7875a0bb0c66bcea7ad16928f5c87fe61e71f
Andrés Canul Bolaños <98436130+AndresCanul@users.noreply.github.com>
2024-02-04 18:57:14 -0600

    Commit Inicial
    Primer commit del proyecto.

---
+++ ProyectoWeb_Martes/ProyectoWeb_Martes/Controllers/InicioController.cs
@@ -0,0 +1,28 @@
+﻿using ProyectoWeb_Martes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.Mvc;
+
+namespace ProyectoWeb_Martes.Controllers
+{
+    public class InicioController : Controller
+    {
+        public ActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        public ActionResult RegistrarUsuario()
+        {
+            return View();
+        }
+
+        public ActionResult PantallaPrincipal()
+        {
+            return View();
+        }
+    }
+}
