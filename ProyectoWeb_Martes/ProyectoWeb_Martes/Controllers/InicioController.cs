3aa20b5e53131b083566acfb6f580d9cd274d950
Andrés Canul Bolaños <98436130+AndresCanul@users.noreply.github.com>
2024-02-17 22:00:03 -0600

    Actualizacion

--- ProyectoWeb_Martes/ProyectoWeb_Martes/Controllers/InicioController.cs
+++ ProyectoWeb_Martes/ProyectoWeb_Martes/Controllers/InicioController.cs
@@ -1,4 +1,4 @@
-﻿using ProyectoWeb_Martes.Models;
+﻿using ProyectoWeb_Martes.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +10,43 @@ namespace ProyectoWeb_Martes.Controllers
 {
     public class InicioController : Controller
     {
+        [HttpGet]
         public ActionResult IniciarSesion()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult IniciarSesion(Usuario entidad)
+        {
+            return RedirectToAction("PantallaPrincipal", "Inicio");
+        }
+
+        [HttpGet]
         public ActionResult RegistrarUsuario()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult RegistrarUsuario(Usuario entidad)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RecuperarAcceso()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RecuperarAcceso(Usuario entidad)
+        {
+            return View();
+        }
+
+        [HttpGet]
         public ActionResult PantallaPrincipal()
         {
             return View();
---
+++ ProyectoWeb_Martes/ProyectoWeb_Martes/Estilos/js/RegistroUsuarios.js
@@ -0,0 +1,18 @@
+﻿function ConsultarNombre() {
+
+    let identification = $("#Identificacion").val();
+
+    $.ajax({
+        url: "https://apis.gometa.org/cedulas/" + identification,
+        method: "GET",
+        datatype: "json",
+        success: function (response)
+        {
+            $("#Nombre").val(response.nombre);
+        },
+        error: function (response)
+        {
+
+        }
+    })
+}
